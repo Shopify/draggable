3b0601007033f9c348b500448223cfd1a26670bb
Max Hoffmann <tsov@me.com>
2017-10-12 23:44:18 -0400

    Export sensors and plugins namespaced

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import AbstractEvent from 'shared/AbstractEvent';
 
+import * as Sensors from './Draggable/Sensors';
+import * as Plugins from './Plugins';
+
 import Draggable from './Draggable';
 import Droppable from './Droppable';
 import Swappable from './Swappable';
 import Sortable from './Sortable';
 
-import {Snappable, Collidable} from './Plugins';
-
-import ForceTouchSensor from './Draggable/Sensors/ForceTouchSensor';
-
 export {
-  Draggable,
-  Sortable,
-  Swappable,
-  Droppable,
-  Snappable,
-  Collidable,
-  ForceTouchSensor,
   AbstractEvent as BaseEvent,
+  Sensors,
+  Plugins,
+
+  Draggable,
+  Droppable,
+  Swappable,
+  Sortable,
 };

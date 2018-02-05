9067eb8a907b17c42e2213d755d1eb8b97758dcc
beefchimi <curtdulmage@gmail.com>
2018-02-04 21:58:15 -0500

    remove all examples src and tooling files so GH does fail due to liquid-like tags

--- examples/src/scripts/utils/flip-sign.js
+++
@@ -1,9 +0,0 @@
-export default function flipSign(number) {
-  if (Math.sign(number) === 1) {
-    return -Math.abs(number);
-  } else if (Math.sign(number) === -1) {
-    return Math.abs(number);
-  } else {
-    return 0;
-  }
-}

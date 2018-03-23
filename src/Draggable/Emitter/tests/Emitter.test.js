5e3f4bac4b5142da761c891b88d3dd36c7316a93
Max Hoffmann <tsov@me.com>
2018-03-22 21:30:42 -0400

    Use console.error instead of throwing error

--- src/Draggable/Emitter/tests/Emitter.test.js
+++ src/Draggable/Emitter/tests/Emitter.test.js
@@ -56,20 +56,28 @@ describe('Emitter', () => {
     });
 
     it('catches errors from listeners and re-throws at the end of the trigger phase', () => {
+      const consoleErrorSpy = jest.fn();
+
       const testEvent = new TestEvent({});
+      const error = new Error('Error');
       const callbacks = [
         jest.fn(),
         () => {
-          throw new Error('Error');
+          throw error;
         },
         jest.fn(),
       ];
 
+      /* eslint-disable no-console */
+      console.error = consoleErrorSpy;
+      /* eslint-enable no-console */
+
       emitter.on('event', ...callbacks);
 
-      expect(() => {
-        emitter.trigger(testEvent);
-      }).toThrow(Error);
+      emitter.trigger(testEvent);
+
+      expect(consoleErrorSpy).toHaveBeenCalled();
+      expect(consoleErrorSpy).toHaveBeenCalledWith("Draggable caught errors while triggering 'event'", [error]);
 
       expect(callbacks[0]).toHaveBeenCalled();
       expect(callbacks[2]).toHaveBeenCalled();

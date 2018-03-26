6907083d950e5574ed1bff88a1268f671ed15508
Max Hoffmann <tsov@me.com>
2018-03-25 21:34:38 -0400

    Fix legacy bundle

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import AbstractPlugin from 'shared/AbstractPlugin';
 import * as Sensors from './Draggable/Sensors';
 import * as Plugins from './Plugins';
 
+export {AbstractEvent as BaseEvent, AbstractPlugin as BasePlugin};
+
 export {default as Draggable} from './Draggable';
 export {default as Droppable} from './Droppable';
 export {default as Swappable} from './Swappable';
 export {default as Sortable} from './Sortable';
 
-export {AbstractEvent as BaseEvent, AbstractPlugin as BasePlugin, Sensors, Plugins};
+export {Sensors, Plugins};

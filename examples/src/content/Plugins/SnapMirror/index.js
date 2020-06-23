eb8cb4bc889b1205b4454835be1a1e3b33a91eae
JuFeng Zhang <zjffun@gmail.com>
2020-06-23 21:44:53 +0800

    add an other demo

--- examples/src/content/Plugins/SnapMirror/index.js
+++ examples/src/content/Plugins/SnapMirror/index.js
@@ -14,15 +14,100 @@ function getNearestPoint(point) {
   };
 }
 
+function getNearestPointForSky(point, points) {
+  let result = point;
+  let distance = Infinity;
+
+  points.forEach((poi) => {
+    if (
+      !(
+        point.x < poi.x + poi.range[1] &&
+        point.x > poi.x - poi.range[3] &&
+        point.y > poi.y - poi.range[0] &&
+        point.y < poi.y + poi.range[2]
+      )
+    ) {
+      return;
+    }
+    const tempDistance = (point.x - poi.x) ** 2 + (point.y - poi.y) ** 2;
+    if (tempDistance < distance) {
+      result = poi;
+      distance = tempDistance;
+    }
+  });
+
+  // console.log(points);
+  return result;
+}
+
+function initSky() {
+  const container = document.querySelector('.sky');
+
+  const points = [];
+  let pointerStart = {x: 0, y: 0};
+  let mirrorStart = {x: 0, y: 0};
+  // const offset = {x: 0, y: 0};
+
+  const draggable = new Draggable([container], {
+    draggable: '.sky__item',
+    mirror: {
+      constrainDimensions: true,
+    },
+  });
+
+  draggable.on('mirror:created', (evt) => {
+    const boundingClientRect = evt.source.getBoundingClientRect();
+
+    mirrorStart = {
+      x: boundingClientRect.x,
+      y: boundingClientRect.y,
+    };
+    pointerStart = {
+      x: evt.sensorEvent.clientX,
+      y: evt.sensorEvent.clientY,
+    };
+
+    [...document.querySelectorAll('.star')].forEach((star) => {
+      const rect = star.getBoundingClientRect();
+      let range = [15, 25, 25, 15];
+      if (star.classList.contains('star1')) {
+        range = [Infinity, Infinity, Infinity, Infinity];
+      }
+      points.push({x: rect.x + 15 - mirrorStart.x, y: rect.y + 15 - mirrorStart.y, range});
+    });
+  });
+
+  draggable.on('mirror:move', (evt) => {
+    evt.cancel();
+
+    requestAnimationFrame(() => {
+      const {clientX, clientY} = evt.sensorEvent;
+      const nearestPoint = getNearestPointForSky(
+        {
+          x: clientX - pointerStart.x,
+          y: clientY - pointerStart.y,
+        },
+        points,
+      );
+      const translate = {
+        x: mirrorStart.x + nearestPoint.x,
+        y: mirrorStart.y + nearestPoint.y,
+      };
+
+      evt.mirror.style.transform = `translate3d(${translate.x}px, ${translate.y}px, 0px)`;
+    });
+  });
+}
+
 export default function PluginsSnapMirror() {
-  const container = document.querySelector('#SnapMirror');
+  const container = document.querySelector('.box');
 
   let pointerStart = {x: 0, y: 0};
   let mirrorStart = {x: 0, y: 0};
   // const offset = {x: 0, y: 0};
 
   const draggable = new Draggable([container], {
-    draggable: '.box',
+    draggable: '.box__item',
     mirror: {
       constrainDimensions: true,
     },
@@ -58,6 +143,9 @@ export default function PluginsSnapMirror() {
       evt.mirror.style.transform = `translate3d(${translate.x}px, ${translate.y}px, 0px)`;
     });
   });
+
+  // demo sky
+  initSky();
 
   return draggable;
 }

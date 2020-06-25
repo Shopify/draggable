da55fc5bf00b8f316e0e319911c916d1f9c3e2c7
Resynth <resynth1943@tutanota.com>
2020-06-25 12:47:31 +0000

    Update examples-app.js

--- examples/src/scripts/examples-app.js
+++ examples/src/scripts/examples-app.js
@@ -1,10 +1,5 @@
-import Analytics from '../components/Analytics';
 import MobileNav from '../components/MobileNav';
 import Content from '../content';
-
-// Initialize Google Analytics
-const gtagTracking = new Analytics('UA-107063633-1');
-gtagTracking.init();
 
 // Initialize navigation
 const mobileNav = new MobileNav(document.getElementById('MobileNavActivator'));

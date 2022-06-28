722c717a6cbdcc03446f38754d9d95b09e218aa7
Henrique Ramos <henriquealberone@outlook.com>
2022-06-28 00:58:35 -0300

    chore: add allowDeclareFields to Babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-typescript', { allowDeclareFields: true }],
+  ],
 };

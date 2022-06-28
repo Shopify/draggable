246b14dc7777203f29666cf784cf2c60d688352e
Henrique Ramos <henriquealberone@outlook.com>
2022-06-27 22:48:27 -0300

    chore: port Plugins to TypeScript

---
+++ src/Plugins/index.ts
@@ -0,0 +1,17 @@
+export {default as Collidable} from './Collidable';
+export {
+  default as ResizeMirror,
+  defaultOptions as defaultResizeMirrorOptions,
+  ResizeMirrorOptions,
+} from './ResizeMirror';
+export {default as Snappable} from './Snappable';
+export {
+  default as SwapAnimation,
+  defaultOptions as defaultSwapAnimationOptions,
+  SwapAnimationOptions,
+} from './SwapAnimation';
+export {
+  default as SortAnimation,
+  defaultOptions as defaultSortAnimationOptions,
+  SortAnimationOptions,
+} from './SortAnimation';

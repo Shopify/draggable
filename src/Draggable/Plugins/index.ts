3b6ae6e13bff90e8181514ca3563fd2d13bfcbfe
Henrique Ramos <henriquealberone@outlook.com>
2022-06-27 22:45:30 -0300

    chore: port Draggable/Plugins to TypeScript

---
+++ src/Draggable/Plugins/index.ts
@@ -0,0 +1,8 @@
+export {
+  default as Announcement,
+  defaultOptions as defaultAnnouncementOptions,
+  AnnouncementOptions,
+} from './Announcement';
+export {default as Focusable, FocusableOptions} from './Focusable';
+export {default as Mirror, defaultOptions as defaultMirrorOptions, MirrorOptions} from './Mirror';
+export {default as Scrollable, defaultOptions as defaultScrollableOptions, ScrollableOptions} from './Scrollable';

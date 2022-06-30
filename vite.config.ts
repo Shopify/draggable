62dad65a805038c3474f431de799e2a782256a5b
Henrique Ramos <henriquealberone@outlook.com>
2022-06-30 01:48:48 -0300

    feat: add declaration file rollup to vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,26 @@ import envCompatible from 'vite-plugin-env-compatible';
 import {viteCommonjs} from '@originjs/vite-plugin-commonjs';
 import {createHtmlPlugin} from 'vite-plugin-html';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^~/,
-        replacement: '',
-      },
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-    extensions: ['.mjs', '.js', '.ts', '.json'],
+  plugins: [
+    dts({
+      rollupTypes: true,
+      outputDir: 'lib',
+      entryRoot: 'src/index.ts'
+    }),
+    viteCommonjs(),
+    envCompatible(),
+    createHtmlPlugin(),
+    tsconfigPaths()
+  ],
+  build: {
+    outDir: 'lib',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'draggable',
+      fileName: (format) => `draggable.${format}.js`
+    }
   },
-  plugins: [viteCommonjs(), envCompatible(), createHtmlPlugin(), tsconfigPaths()],
-  build: {},
 });

117b2e37b906e8024820c4e2204805206a090337
Henrique Ramos <henriquealberone@outlook.com>
2022-07-01 01:10:24 -0300

    chore: separate type exports from regular exports in src/Plugins

--- src/Plugins/index.ts
+++ src/Plugins/index.ts
@@ -2,16 +2,17 @@ export { default as Collidable } from './Collidable';
 export {
   default as ResizeMirror,
   defaultOptions as defaultResizeMirrorOptions,
-  ResizeMirrorOptions,
 } from './ResizeMirror';
 export { default as Snappable } from './Snappable';
 export {
   default as SwapAnimation,
   defaultOptions as defaultSwapAnimationOptions,
-  SwapAnimationOptions,
 } from './SwapAnimation';
 export {
   default as SortAnimation,
   defaultOptions as defaultSortAnimationOptions,
-  SortAnimationOptions,
 } from './SortAnimation';
+
+export type { ResizeMirrorOptions } from './ResizeMirror';
+export type { SwapAnimationOptions } from './SwapAnimation';
+export type { SortAnimationOptions } from './SortAnimation';

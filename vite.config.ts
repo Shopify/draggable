e9e7fc2e62845e7729cd6be72a4227081d45e589
Henrique Ramos <henriquealberone@outlook.com>
2022-07-01 01:08:11 -0300

    chore: add .d.ts rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
 /// <reference types="vite/client" />
 
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
 import envCompatible from 'vite-plugin-env-compatible';
-import {viteCommonjs} from '@originjs/vite-plugin-commonjs';
-import {createHtmlPlugin} from 'vite-plugin-html';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import dts from 'vite-plugin-dts'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
     dts({
       rollupTypes: true,
       outputDir: 'lib',
-      entryRoot: 'src/index.ts'
+      entryRoot: 'src/index.ts',
     }),
     viteCommonjs(),
     envCompatible(),
     createHtmlPlugin(),
-    tsconfigPaths()
+    tsconfigPaths(),
   ],
   build: {
     outDir: 'lib',
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'draggable',
-      fileName: (format) => `draggable.${format}.js`
-    }
+      fileName: (format) => `draggable.${format}.js`,
+    },
   },
 });

5972c9935aad6bb97998dca108b831ca587d443b
Henrique Ramos <henriquealberone@outlook.com>
2022-07-31 11:05:22 -0300

    chore: port examples/src/components/PillSwitch to Svelte

---
+++ examples/src/components/PillSwitch/PillSwitch.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Pattern from '../Pattern/Pattern.svelte';
+
+	let on: boolean = false;
+	let heading: string;
+</script>
+
+<article class="pill-switch" class:pill-switch--on={on}>
+	<div class="pill-switch__track">
+		<Pattern variant="halftone" />
+	</div>
+
+	<span class="pill-switch__control">
+		<p class="heading heading--jumbo text-no-select">
+			{heading}
+		</p>
+	</span>
+</article>
+
+<style lang="scss">
+	@use 'styles';
+</style>

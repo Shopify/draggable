ec3870f2bc5372f65a70824c118de467ee0bb39f
Henrique Ramos <henriquealberone@outlook.com>
2022-07-31 10:52:15 -0300

    chore: port examples/src/components/Block to Svelte

---
+++ examples/src/components/Block/Block.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import classNames from 'classnames';
+	import Pattern from '../Pattern/Pattern.svelte';
+
+	export let draggable: boolean = false;
+	export let variant: 'shell' | 'hollow' | 'stripes' | undefined = undefined;
+	export let classes: string | string[] | undefined = undefined;
+
+	export let label: string = '';
+</script>
+
+<span
+	class={classNames('block', classes)}
+	class:block--draggable={draggable}
+	title={draggable ? 'Click to drag' : ''}
+>
+	<div
+		class="block__content"
+		class:block__content--shell={variant === 'shell'}
+		class:block__content--hollow={variant === 'hollow'}
+		class:block__content--stripes={variant === 'stripes'}
+	>
+		<h3 class="heading heading--size2 text-no-select">{label}</h3>
+
+		{#if draggable}
+			<Pattern variant="halftone" />
+			<Pattern variant="placed" />
+		{/if}
+	</div>
+</span>
+
+<style lang="scss">
+	@use 'styles';
+</style>

2a5abf9e940c09f44b5b6862ca1efd79a461bcd2
Henrique Ramos <henriquealberone@outlook.com>
2022-07-31 11:03:23 -0300

    chore: port examples/src/components/Hamburger to Svelte

---
+++ examples/src/components/Hamburger/Hamburger.svelte
@@ -0,0 +1,19 @@
+<script type="ts">
+	let expanded: boolean = false;
+</script>
+
+<button
+	type="button"
+	id="mobile-nav-activator"
+	class="hamburger"
+	class:hamburger__expanded={expanded}
+	aria-controls="sidebar"
+>
+	<div class="hamburger-bun">
+		<div class="hamburger-patty" />
+	</div>
+</button>
+
+<style lang="scss">
+	@use './Hamburger.scss';
+</style>

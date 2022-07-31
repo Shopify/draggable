004dc7d57f224b7e89f09182d2b945a89fa88944
Henrique Ramos <henriquealberone@outlook.com>
2022-07-31 11:05:06 -0300

    chore: move examples/src/components/Patterns to examples/src/components/Pattern

---
+++ examples/src/components/Pattern/Pattern.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	export let variant: 'stripes' | 'halftone' | 'placed' = 'stripes';
+</script>
+
+<div class={classNames('pattern', `pattern--${variant}`)} />
+
+<style lang="scss">
+	@use 'styles';
+</style>

7f8089fb69605bd3e42b7b08da2a37ca03995c29
Henrique Ramos <henriquealberone@outlook.com>
2022-07-31 11:04:26 -0300

    chore: port examples/src/components/PaperStackItem to Svelte

---
+++ examples/src/components/PaperStack/PaperStackItem.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import classNames from 'classnames';
+	import Pattern from '../Pattern/Pattern.svelte';
+
+	export let draggable: boolean = false;
+	export let classes: string | string[] | undefined;
+	export let heading: string;
+</script>
+
+<li
+	class={classNames('paper-stack__item', classes)}
+	class:paper__stack-item--draggable={draggable}
+	tabIndex={draggable ? 1 : 0}
+>
+	<div class="paper-stack__content">
+		<h4 class="heading text-no-select paper-stack__heading">{heading}</h4>
+		{#if draggable}
+			<Pattern variant="halftone" />
+			<Pattern variant="placed" />
+		{/if}
+	</div>
+</li>
+
+<style lang="scss">
+	@use 'styles';
+</style>

bb99ddd4a11c162360f9ff732d820026af59c1c4
Henrique Ramos <henriquealberone@outlook.com>
2022-08-01 16:15:03 -0300

    chore: port examples/src/routes/Swappable to svelte

---
+++ examples/src/routes/Swappable/GridLayout/GridLayout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import { Swappable, Plugins } from '@draggable';
+	import Block from 'src/components/Block/Block.svelte';
+
+	let container: HTMLElement;
+
+	onMount(() => {
+		new Swappable([container], {
+			draggable: '.block--draggable',
+			mirror: {
+				appendTo: container,
+				constrainDimensions: true
+			},
+			plugins: [Plugins.ResizeMirror]
+		});
+	});
+</script>
+
+<section>
+	<article bind:this={container} class="block-layout block-layout--typeGrid">
+		<div class="block__wrapper"><Block label="one" classes="block--1" draggable /></div>
+		<div class="block__wrapper"><Block label="two" classes="block--2" draggable /></div>
+		<div class="block__wrapper"><Block label="three" classes="block--3" /></div>
+		<div class="block__wrapper"><Block label="four" classes="block--4" draggable /></div>
+		<div class="block__wrapper"><Block label="five" classes="block--5" draggable /></div>
+		<div class="block__wrapper"><Block label="six" classes="block--6" draggable /></div>
+		<div class="block__wrapper"><Block label="seven" classes="block--7" /></div>
+	</article>
+</section>
+
+<style lang="scss">
+	@use 'styles';
+</style>

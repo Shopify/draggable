f2f4deef2f42e72c3bd72aa5d9b98764d880441f
Henrique Ramos <henriquealberone@outlook.com>
2022-08-11 02:22:59 -0300

    feat: port examples/swappable/grid-layout to Svelte

--- examples/src/routes/examples/swappable/grid-layout/index.svelte
+++ examples/src/routes/examples/swappable/grid-layout/index.svelte
@@ -1,25 +1,39 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import { Swappable, Plugins } from '@draggable';
 	import Block from '@src/components/Block/Block.svelte';
+	import { browser } from '$app/env';
+
+	import './styles/index.scss';
+	import PageHeader from '@src/components/PageHeader/PageHeader.svelte';
 
 	let container: HTMLElement;
 
-	onMount(() => {
-		new Swappable([container], {
-			draggable: '.block--draggable',
-			mirror: {
-				appendTo: container,
-				constrainDimensions: true
-			},
-			plugins: [Plugins.ResizeMirror]
-		});
+	onMount(async () => {
+		if (browser) {
+			const { Swappable, Plugins } = await import('@draggable');
+
+			new Swappable([container], {
+				draggable: '.block--draggable',
+				mirror: {
+					appendTo: container,
+					constrainDimensions: true
+				},
+				plugins: [Plugins.ResizeMirror]
+			});
+		}
 	});
 </script>
 
-<section>
-	<article bind:this={container} class="block-layout block-layout--typeGrid">
+<PageHeader
+	id="GridLayout"
+	section="Swappable"
+	child="Grid Layout"
+	subheading="Draggable children do not need to be direct descendants of their container. Wrapper elements can be used to maintain layout and simplify styling."
+/>
+
+<section class="grid-layout">
+	<article bind:this={container} class="block-layout block-layout--grid">
 		<div class="block__wrapper"><Block label="one" classes="block--1" draggable /></div>
 		<div class="block__wrapper"><Block label="two" classes="block--2" draggable /></div>
 		<div class="block__wrapper"><Block label="three" classes="block--3" /></div>
@@ -29,7 +43,3 @@
 		<div class="block__wrapper"><Block label="seven" classes="block--7" /></div>
 	</article>
 </section>
-
-<style lang="scss">
-	@use 'styles';
-</style>

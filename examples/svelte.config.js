19dfc827df3f1294e53a5cedf2f39a7e20f91298
Henrique Ramos <henriquealberone@outlook.com>
2022-08-11 02:29:19 -0300

    chore: replace sveltePreprocess with vitePreprocess

--- examples/svelte.config.js
+++ examples/svelte.config.js
@@ -1,32 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-import { fileURLToPath, URL } from 'url';
-
-const scssAliases = (aliases) => (url) => {
-	for (const [alias, aliasPath] of Object.entries(aliases)) {
-		if (url.indexOf(alias) === 0) {
-			return {
-				file: url.replace(alias, aliasPath)
-			};
-		}
-	}
-	return url;
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		scss: {
-			importer: [
-				scssAliases({
-					'@src': fileURLToPath(new URL('./src', import.meta.url))
-				})
-			]
-		}
-	}),
-
 	kit: {
 		adapter: adapter(),
 		prerender: {
@@ -35,6 +10,11 @@ const config = {
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
+		}
+	},
+	vitePlugin: {
+		experimental: {
+			useVitePreprocess: true
 		}
 	}
 };

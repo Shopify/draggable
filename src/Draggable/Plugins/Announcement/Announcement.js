12327f49f6c16ff8e4843dcdb840790fccea582d
Michael Voříšek <mvorisek@mvorisek.cz>
2022-10-08 01:30:07 +0200

    Do not set fixed (possible duplicate) ID

--- src/Draggable/Plugins/Announcement/Announcement.js
+++ src/Draggable/Plugins/Announcement/Announcement.js
@@ -157,7 +157,7 @@ function announce(message, {expire}) {
 function createRegion() {
   const element = document.createElement('div');
 
-  element.setAttribute('id', 'draggable-live-region');
+  element.classList.add('draggable-live-region');
   element.setAttribute(ARIA_RELEVANT, 'additions');
   element.setAttribute(ARIA_ATOMIC, 'true');
   element.setAttribute(ARIA_LIVE, 'assertive');

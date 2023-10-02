773496192fc9a56f6cd24ec7a45f34c79aec4a6d
Max Hoffmann <tsov@me.com>
2023-10-02 13:23:07 -0700

    Convert DragEvent tests to typescript

---
+++ .changeset/neat-rockets-lick.md
@@ -0,0 +1,5 @@
+---
+'@shopify/draggable': patch
+---
+
+Converts DragEvent tests to typescript
--- src/Draggable/Sensors/SensorEvent/SensorEvent.ts
+++ src/Draggable/Sensors/SensorEvent/SensorEvent.ts
@@ -1,12 +1,12 @@
 import AbstractEvent from 'shared/AbstractEvent';
 
 interface SensorEventData {
-  originalEvent: Event;
   clientX: number;
   clientY: number;
   target: HTMLElement;
   container: HTMLElement;
   originalSource: HTMLElement;
+  originalEvent?: Event;
   pressure?: number;
 }
 
